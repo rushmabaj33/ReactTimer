0b8e380e86a5c263f2974723ad9bfaed523bfc00
Rushma Bajracharya <bajracharya@klimakoenner.de>
2017-06-12 14:26:46 +0200

    clock component added

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,9 +1,12 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
-	render: function () {
-		return <p>Countdown.jsx</p>
-	}
+  render: function () {
+    return (
+    	<Clock totalSeconds={129}/>
+    );
+  }
 });
 
 module.exports = Countdown;
--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -2,30 +2,30 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 var Navigation = () => {
-	return (
-		<div className="top-bar">
-			<div className="top-bar-left">
-				<ul className="menu">
-					<li className="menu-text">
-						React Timer App
-					</li>
-					<li>
-						<IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
-					</li>
-					<li>
-						<Link to="/countdown" activeClassName="active-link">Countdown</Link>
-					</li>
-				</ul>
-			</div>
-			<div className="top-bar-right">
-				<ul className="menu">
-					<li className="menu-text">
-						Created by <a href="https://www.linkedin.com/in/rushma-bajracharya-79a633a1/" target="_blank">Rushma Bajracharya</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/countdown" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="https://www.linkedin.com/in/rushma-bajracharya-79a633a1/" target="_blank">Rushma Bajracharya</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 module.exports = Navigation;
